ebc74bc1cd0c352da05d6b805dfccc65f63650df
Markingit <15249287235@163.com>
2020-05-22 15:54:32 +0800

    项目准备

---
+++ src/store/modules/book.js
@@ -0,0 +1,5 @@
+const book = {
+
+}
+
+export default book
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="icon-bookmark"></span>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Epub from 'epubjs'
+global.epub = Epub
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  // components: {
+    
+  // },
+  // mounted () {
+  //   this.book = new Epub('/2018_Book_AgileProcessesInSoftwareEngine.epub')
+  //   console.log(this.book)
+  //   this.book.renderTo('read', {
+  //     width: window.innerWidth,
+  //     height: window.innerHeight
+  //   }).display()
+
+  // }
 }
 </script>
